889f66c759d953bc0a4706dde0cf7a06b0c1c0bb
isadoraora <isa.mdeo@gmail.com>
2019-12-13 23:12:27 -0200

    atualizando api-doc

--- doc/api_project.js
+++ doc/api_project.js
@@ -1,7 +1,7 @@
 define({
-  "name": "API Customer Experience",
+  "name": "API - Pesquisa de Satisfação(Customer Experience)",
   "version": "0.1.0",
-  "description": "ApiDoc sobre API - Cálculo de Média do Customer Experience",
+  "description": "ApiDoc sobre API - Cálculo de Média dos analistas de atendimento",
   "title": "CX - apiDoc - Isadora",
   "url": "https://github.com/isadoraora/ProjetoCX",
   "sampleUrl": false,
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-12-12T23:28:10.442Z",
+    "time": "2019-12-14T01:11:05.307Z",
     "url": "http://apidocjs.com",
     "version": "0.19.0"
   }
